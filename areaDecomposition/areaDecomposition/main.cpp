8f7969eae97e6302c5cad9fcd4621824b98ccde8
csringam <cal9ten@gmail.com>
2025-06-15 22:41:13 -0400

    Initial set up of area decomposition work

---
+++ areaDecomposition/areaDecomposition/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+int main() {
+	int input{ 0 };
+	std::cout << "Hello World!" << std::endl;
+	std::cin >> input;
+	std::cout << "You entered: " << input << std::endl;
+	return 0;
+}
